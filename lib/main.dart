d64fb53f04a4fb6158148a3b5b7a9acdd1bedfdd
rahulsharma001 <rahul.sharma.976822@gmail.com>
2018-11-06 18:06:03 +0530

    a

---
+++ lib/main.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/animation.dart';
+import 'package:flutter/material.dart';
+
+class LogoApp extends StatefulWidget {
+  _LogoAppState createState() => _LogoAppState();
+}
+
+class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin {
+  Animation<double> animation;
+  AnimationController controller;
+  
+
+  initState() {
+    super.initState();
+    controller =
+        AnimationController(duration: const Duration(seconds: 3), vsync: this);
+    animation = Tween(begin: 500.0, end: -90.0).animate(controller)
+      ..addListener(() {
+        setState(() {
+          // the state that has changed here is the animation object’s value
+        });
+      });
+    controller.repeat();
+  }
+
+  Widget build(BuildContext context) {
+        final Matrix4 transform = new Matrix4.rotationZ(90 * 2.0);
+
+    return Container(
+      // margin: EdgeInsets.symmetric(vertical: 10.0),
+      // height: animation.value,
+      // width: animation.value,
+      // child: FlutterLogo(),
+
+      color: Colors.white,
+      child: Stack(
+        textDirection: TextDirection.ltr,
+        children: <Widget>[
+          Positioned(
+            child: Material(
+              child: Transform(
+                transform:transform ,
+                child: Icon(
+                  Icons.airport_shuttle,
+                  size: 80.0,
+                  textDirection: TextDirection.rtl,
+                ),
+              ),
+            ),
+            left: animation.value,
+            top: 40.0,
+          ),
+        ],
+      ),
+    );
+  }
+
+  dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+}
+
+void main() {
+  runApp(LogoApp());
+}
